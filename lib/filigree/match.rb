2926422e6a6b25359fd1d20b3127783c785f7071
Chris Wailes <chris.wailes@gmail.com>
2013-05-05 21:23:20 -0600

    Added support for wildcards and tuples.

--- lib/filigree/match.rb
+++ lib/filigree/match.rb
@@ -21,12 +21,12 @@ class MatchError < RuntimeError; end
 # Methods #
 ###########
 
-def match(object, &block)
+def match(*objects, &block)
 	me = MatchEnvironment.new
 	
 	me.instance_exec &block
 	
-	me.find_match object
+	me.find_match objects
 end
 
 #######################
@@ -36,15 +36,16 @@ end
 class MatchEnvironment
 	def initialize
 		@patterns = Array.new
+		@deferred = Array.new
 	end
 	
-	def find_match(object)
+	def find_match(objects)
 		found	= false
 		result	= nil
 		
 		@patterns.each do |pattern|
-			if found = pattern.match?(object)
-				result = pattern.(object)
+			if found = pattern.match?(objects)
+				result = pattern.(objects)
 				break
 			end
 		end
@@ -52,24 +53,45 @@ class MatchEnvironment
 		if found then result else raise MatchError end
 	end
 	
-	def with(pattern, guard = nil, &block)
-		@patterns << MatchPattern.new(pattern, guard, block)
+	def with(*pattern, &block)
+		guard = if pattern.last.is_a?(Proc) then pattern.pop end 
+		
+		@patterns << (mp = MatchPattern.new(pattern, guard, block))
+		
+		if block
+			@deferred.each { |p| p.block = block }
+			@deferred.clear
+			
+		else
+			@deferred << mp
+		end
 	end
 end
 
 class MatchPattern
+	attr_writer :block
+	
 	def initialize(pattern, guard, block)
 		@pattern	= pattern
 		@guard	= guard
 		@block	= block
 	end
 	
-	def call(object)
-		@block.(object)
+	def call(objects)
+		@block.(*objects)
 	end
 	
-	def match?(object)
-		(@pattern == :_ || object == @pattern) and (@guard.nil? or @guard.(object))
+	def match?(objects)
+		if objects.length == @pattern.length
+			objects.zip(@pattern).inject(nil) do |_, pair|
+				o, p = pair
+				
+				if p == :_ || o == p then true else break false end
+			end
+			
+		else
+			@pattern.length == 1 and @pattern.first == :_
+		end && (@guard.nil? or @guard.(*objects))
 	end
 end
 
--- test/tc_match.rb
+++ test/tc_match.rb
@@ -18,15 +18,29 @@ require 'filigree/match'
 #######################
 
 class MatchTester < Test::Unit::TestCase
+	
 	def setup
 		
 	end
 	
-	def match_tester_simple(o)
+	###########
+	# Helpers #
+	###########
+	
+	def match_tester_deferred(o)
 		match o do
-			with(1) { :one   }
-			with(2) { :two   }
-			with(3) { :three }
+			with(1)
+			with(2) { :NUM }
+			with(:a)
+			with(:b) { :SYM }
+		end
+	end
+	
+	def match_tester_guard(o)
+		match o do
+			with(:_, ->(n) { n < 0 })	{ :NEG  }
+			with(0)					{ :ZERO }
+			with(:_, ->(n) { n > 0 })	{ :POS  }
 		end
 	end
 	
@@ -39,6 +53,31 @@ class MatchTester < Test::Unit::TestCase
 		end
 	end
 	
+	def match_tester_simple(o)
+		match o do
+			with(1) { :one   }
+			with(2) { :two   }
+			with(3) { :three }
+		end
+	end
+	
+	def match_tester_tuple(*touple)
+		match *touple do
+			with(1, 2)	{ :FOO }
+			with(3, 4)	{ :BAR }
+			with(5)		{ :BAF }
+		end
+	end
+	
+	def match_tester_tuple_wildcard(*touple)
+		match *touple do
+			with(1)
+			with(2,3)		{ :DEF }
+			with(4, :_)	{ :PART_WILD }
+			with(:_)		{ :WILD }
+		end
+	end
+	
 	def match_tester_wildcard(o)
 		match o do
 			with(1)	{ 1 }
@@ -47,13 +86,9 @@ class MatchTester < Test::Unit::TestCase
 		end
 	end
 	
-	def match_tester_guard(o)
-		match o do
-			with(:_, ->(n) { n < 0 })	{ :NEG  }
-			with(0)					{ :ZERO }
-			with(:_, ->(n) { n > 0 })	{ :POS  }
-		end
-	end
+	#########
+	# Tests #
+	#########
 	
 	def test_constants
 		assert_equal :one,   match_tester_simple(1)
@@ -67,10 +102,39 @@ class MatchTester < Test::Unit::TestCase
 		assert_equal :one,    match_tester_mixed(1)
 	end
 	
+	def test_deferred_block
+		assert_equal :NUM, match_tester_deferred(1)
+		assert_equal :NUM, match_tester_deferred(2)
+		
+		assert_equal :SYM, match_tester_deferred(:a)
+		assert_equal :SYM, match_tester_deferred(:b)
+	end
+	
 	def test_guards
 		assert_equal :NEG,  match_tester_guard(-5)
 		assert_equal :ZERO, match_tester_guard(0)
 		assert_equal :POS,  match_tester_guard(6)
+	end
+	
+	def test_tuple_wildcard
+		assert_equal :DEF, match_tester_tuple_wildcard(1)
+		assert_equal :DEF, match_tester_tuple_wildcard(2, 3)
+		
+		assert_equal :PART_WILD, match_tester_tuple_wildcard(4, 1)
+		assert_equal :PART_WILD, match_tester_tuple_wildcard(4, :cat)
+		
+		assert_equal :WILD, match_tester_tuple_wildcard(5)
+		assert_equal :WILD, match_tester_tuple_wildcard(5, 6)
+		assert_equal :WILD, match_tester_tuple_wildcard(5, 6, 7)
+	end
+	
+	def test_tuples
+		assert_equal :FOO, match_tester_tuple(1, 2)
+		assert_equal :BAR, match_tester_tuple(3, 4)
+		assert_equal :BAF, match_tester_tuple(5)
+		
+		assert_raise(MatchError) { match_tester_tuple(1, 2, 3) }
+		assert_raise(MatchError) { match_tester_tuple(6)		}
 	end
 	
 	def test_wildcard_pattern
