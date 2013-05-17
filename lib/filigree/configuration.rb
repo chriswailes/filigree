e40de7c8c2e4be9b8382033bd5f90c09ccbe1063
Chris Wailes <chris.wailes@gmail.com>
2013-05-16 23:39:37 -0600

    Added tests cases for and fixed bugs in the Configuration module.  Added support for default values.  Next step is to add support for serialization.

--- lib/filigree/configuration.rb
+++ lib/filigree/configuration.rb
@@ -40,37 +40,55 @@ module Filigree::Configuration
 	attr_accessor :rest
 	
 	def initialize(argv = ARGV.clone)
+		set = self.class.options_long.keys.inject(Hash.new) { |h, option| h[option] = false; h }
+		
 		# Process the command line.
 		while str = argv.shift
 			
 			break if str == '--'
 			
-			if option = find_option str
+			if option = find_option(str)
 				args =
 				if option.arity == -1
-					args.shift (args.index { |s| s[0,1] == '-'})
+					argv.shift (argv.index { |s| s[0,1] == '-'})
 				else
-					args.shift option.arity
+					argv.shift option.arity
 				end
 				
-				value =
 				case option.handler
 				when Array
-					args.zip(options.handler).map { |s, sym| s.send sym }
+					tmp = args.zip(option.handler).map { |s, sym| s.send sym }
 					
-				when Option
-					option.handler.call(*args)
+					if option.arity == 1 and tmp.length == 1
+						self.send("#{option.long}=", tmp.first)
+					else
+						self.send("#{option.long}=", tmp)
+					end
+					
+				when Proc
+					self.send("#{option.long}=", option.handler.call(*args))
 				end
 				
-				self.send("#{option.long}=", value)
+				set[option.long] = true
 			end
 		end
 		
 		# Save the rest of the command line for later.
 		self.rest = argv
 		
+		# Set defaults.
+		set.each do |name, is_set|
+			if not is_set
+				default = self.class.options_long[name].default
+			
+				default = self.instance_exec &default if default.is_a? Proc
+				
+				self.send("#{name}=", default)
+			end
+		end
+		
 		# Check to make sure all the required options are set.
-		self.class.required.each do |option|
+		self.class.required_options.each do |option|
 			if self.send(option).nil?
 				raise ArgumentError, "Option #{option} not set."
 			end
@@ -96,10 +114,13 @@ module Filigree::Configuration
 	module ClassMethods
 		attr_reader :options_long
 		attr_reader :options_short
-		attr_reader :required
 		
 		def auto(name, &block)
 			define_method(name, &block)
+		end
+		
+		def default(val = nil, &block)
+			@next_default = if block then block else val end
 		end
 		
 		def help(str)
@@ -107,7 +128,8 @@ module Filigree::Configuration
 		end
 		
 		def install_icvars
-			@help_string	= nil
+			@help_string	= ''
+			@next_default	= nil
 			@next_required	= false
 			@options_long	= Hash.new
 			@options_short	= Hash.new
@@ -120,13 +142,14 @@ module Filigree::Configuration
 			attr_accessor long.to_sym
 			
 			@options_long[long] = @options_short[short] =
-				Option.new(long, short, @help_string,
+				Option.new(long, short, @help_string, @next_default,
 					if not conversions.empty? then conversions else block end)
 			
 			@required << long.to_sym if @next_required
 			
 			# Reset state between option declarations.
-			@help_string	= nil
+			@help_string	= ''
+			@next_default	= nil
 			@next_required = false
 		end
 		
@@ -137,6 +160,10 @@ module Filigree::Configuration
 				@required += names
 			end
 		end
+
+		def required_options
+			@required
+		end
 		
 		def segment(str, indent, max_length = 80)
 			lines = Array.new
@@ -145,37 +172,23 @@ module Filigree::Configuration
 			str.split(/\s/).each do |word|
 				new_length  = line.length + word.length + indent + 1
 				
-				if  new_length < max_length
-					line += ' ' + word
+				if new_length < max_length
+					line += ' ' if line.length != 0
+					line += word
+					
 				else
 					lines << line
 					line = word
 				end
 			end
 			
-			lines.join("\n" + (indent * ' '))
+			lines << line if not line.empty?
+			
+			lines.join("\n\t" + (' ' * indent))
 		end
 		
 		def usage(str)
 			@usage = str
-		end
-		
-		#######################
-		# Pre-defined Options #
-		#######################
-		
-		option 'help', 'h' do
-			option_names	= @options_long.keys.sort
-			max_length	= option_names.inject(0) { |m, s| if m <= s.length then s.length else m end }
-			segment_indent	= max_length + 3
-			
-			puts "Usage: #{@usage}"
-			puts
-			puts 'Options:'
-			
-			option_names.each do |name|
-				printf "%#{max_length}s - %s\n", name, segment(@options_long[name].help, segment_indent)
-			end
 		end
 		
 		#############
@@ -185,6 +198,30 @@ module Filigree::Configuration
 		class << self
 			def extended(klass)
 				klass.install_icvars
+				
+				#######################
+				# Pre-defined Options #
+				#######################
+		
+				klass.instance_exec do
+					help 'Prints this help message.'
+					option 'help', 'h' do
+						option_names	= @options_long.keys.sort
+						max_length	= option_names.inject(0) { |m, s| if m <= s.length then s.length else m end }
+						segment_indent	= max_length + 3
+			
+						puts "Usage: #{@usage}"
+						puts
+						puts 'Options:'
+			
+						option_names.each do |name|
+							printf "\t% #{max_length}s - %s\n", name, segment(@options_long[name].help, segment_indent)
+						end
+			
+						# Quit the application after printing the help message.
+						exit
+					end
+				end
 			end
 		end
 	end
@@ -193,7 +230,16 @@ module Filigree::Configuration
 	# Inner Classes #
 	#################
 	
-	Option = Struct.new(:long, :short, :help, :handler)
+	Option = Struct.new(:long, :short, :help, :default, :handler)
+	
+	class Option
+		def arity
+			case self.handler
+			when Array	then self.handler.length
+			when Proc		then self.handler.arity 
+			end
+		end
+	end
 	
 	#############
 	# Callbacks #
--- test/tc_configuration.rb
+++ test/tc_configuration.rb
@@ -21,15 +21,86 @@ class ConfigurationTester < Test::Unit::TestCase
 	class TestConfig
 		include Filigree::Configuration
 		
-		auto :foo do
-			:foo
+		help 'Does foo.'
+		default { moo.to_s }
+		option 'foo', 'f' do |s|
+			s
 		end
 		
-		help 'foo'
+		help 'This is a longer help message to test and see how the string segmentation works.  I hope it is long enough.'
+		default 42
+		option 'bar', 'b', :to_i
+		
+		help 'Does baz.'
+		option 'baz', 'z', :to_sym, :to_sym
+		
+		help 'Does moo.'
+		required
+		option 'moo', 'm' do |i|
+			i.to_i * 6
+		end
+		
+		help 'This does zoom'
+		option 'daf', 'd' do |*syms|
+			syms.map { |syms| syms.to_sym }
+		end
+		
+		auto :cow do
+			self.moo / 3
+		end
+	end
+	
+	def setup
+		@defaults = ['--moo', '10']
 	end
 	
 	def test_auto
-		assert_equal :foo, TestConfig.new.foo
-		assert_equal 'foo', TestConfig.new.help_string
+		conf = TestConfig.new @defaults
+		
+		assert_equal 20, conf.cow
+	end
+	
+	def test_defaults
+		conf = TestConfig.new @defaults
+		
+		assert_equal 42, conf.bar
+		assert_equal '60', conf.foo
+	end
+	
+	def test_long_option
+		conf = TestConfig.new @defaults
+		
+		assert_equal 60, conf.moo
+	end
+	
+	def test_proc_handler
+		conf = TestConfig.new (@defaults + ['--foo', 'hello world'])
+		
+		assert_equal 'hello world', conf.foo
+	end
+	
+	def test_required
+		assert_raise(ArgumentError)	{ TestConfig.new([]) }
+		assert_nothing_raised		{ TestConfig.new(@defaults) }
+	end
+	
+	def test_short_option
+		conf = TestConfig.new ['-m', 10]
+		
+		assert_equal 60, conf.moo
+	end
+	
+	def test_splat
+		conf = TestConfig.new (['-d', 'a', 'b', 'c'] + @defaults)
+		
+		assert_equal [:a, :b, :c], conf.daf
+	end
+	
+	def test_symbol_handler
+		conf = TestConfig.new (@defaults + ['-b', '42'])
+		assert_equal 42, conf.bar
+		
+		conf = TestConfig.new (@defaults + ['-z', 'a', 'b'])
+		assert_equal [:a, :b], conf.baz
 	end
 end
